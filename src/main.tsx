50e5f6fac68bab257bd33cb98eec234208952aae
M Hamza <mhamzaaftab1166@gmail.com>
2025-05-01 10:07:00 +0400

    dev tool setup

--- src/main.tsx
+++ src/main.tsx
@@ -7,22 +7,24 @@ import { CssBaseline } from '@mui/material';
 import { theme } from 'theme/theme.ts';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { QueryClientProvider, QueryClient } from "react-query";
+import { QueryClientProvider, QueryClient } from 'react-query';
 import { SnackbarProvider } from 'notistack';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-    <SnackbarProvider maxSnack={3}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
-    </SnackbarProvider>
-    </Provider>
+      <Provider store={store}>
+        <SnackbarProvider maxSnack={3}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <RouterProvider router={router} />
+            {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+          </ThemeProvider>
+        </SnackbarProvider>
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
