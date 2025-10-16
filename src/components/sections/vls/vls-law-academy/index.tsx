8ea770ad2dc046a6c822afc7fba1e291e1fa32b8
Veeravel-S <veeravel.igt@gmail.com>
2025-10-16 22:09:42 +0530

    title changes

--- src/components/sections/vls/vls-law-academy/index.tsx
+++ src/components/sections/vls/vls-law-academy/index.tsx
@@ -9,9 +9,7 @@ import VlsLawAcademyTable from './vlsTable';
 import { handleCSVDownloadData, handleXlsxDownloadData } from 'components/hooks/useExportDataToExcel';
 import { Popup } from 'components/common/Popup';
 import ConfirmAlert from 'components/common/ConfirmAlert';
-import { Typography } from '@mui/material';
-import IconifyIcon from 'components/base/IconifyIcon';
-import dayjs from 'dayjs';
+import VlsAcademyView from './vlsAcademyView';
 
 const VlsLawAcademySection = () => {
     const { data: usersData, isLoading } = useVlsLawAcademyQuery();
@@ -21,7 +19,6 @@ const VlsLawAcademySection = () => {
     const [selectedUserId, setSelectedUserId] = useState<number>(0);
     const [viewModal, setviewModal] = useState(false);
     const [selectedUser, setSelectedUser] = useState<any>(null);
-    const [copied, setcopied] = useState(false);
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearchText(e.target.value);
@@ -50,25 +47,7 @@ const VlsLawAcademySection = () => {
         setviewModal(false);
         setSelectedUser(null);
     }
-    const shareurl: any = [
-        `Name : ${selectedUser?.name ?? '---'} `,
-        `Email : ${selectedUser?.email ?? '---'} `,
-        `Mobile : ${selectedUser?.mobile ?? '---'} `,
-        `Message : ${selectedUser?.message ?? '---'}`,
-        `Registered Date : ${dayjs(selectedUser?.registered_date).format("YYYY-MMMM-DD") ?? '---'}`,
-    ]
 
-    const handleurl = async () => {
-        try {
-            await navigator.clipboard.writeText(shareurl);
-            setcopied(true);
-            setTimeout(() => {
-                setcopied(false);
-            }, 2000);
-        } catch (err) {
-            console.log(err);
-        }
-    };
     if (isLoading) return <PageLoader />;
 
     // console.log("sss" , usersData)
@@ -109,55 +88,7 @@ const VlsLawAcademySection = () => {
             </Popup>
 
             <Popup open={viewModal} onClose={handleCloseViewModal}>
-                <Stack
-                    flexDirection={'column'}
-                    justifyContent={'center'}
-                    alignItems="center"
-                    spacing={2}
-                    width={"600px"}
-                    position={"relative"}
-                    sx={{ padding: 4 }}
-                >
-                    <Typography textAlign={'center'} variant="h4" sx={{ width: '100%' }}>
-                        Vls Law Practice User
-                    </Typography>
-
-                    <Stack flexDirection={"column"} position={"absolute"} bottom={"15px"} right={"10px"} >
-                        <IconifyIcon
-                            onClick={handleurl}
-                            icon="hugeicons:copy-01"
-                            sx={{
-                                cursor: 'pointer',
-                                fontSize: "35px",
-                                backgroundColor: '#47b4ec',
-                                color: '#fff',
-                                borderRadius: '10px',
-                                padding: 0.8,
-                                '&:hover': {
-                                    backgroundColor: '#000000ff',
-                                    color: '#fff',
-                                },
-                            }}
-                        />
-                        <Typography sx={{ fontSize: "10px", fontWeight: "600", textAlign: "center" }}>{copied ? 'Copied' : ''}</Typography>
-                    </Stack>
-                    <Stack width={"100%"} display={"flex"} gap={"5px"} marginTop={"20px"}>
-                        <Stack flexDirection={"column"} width={"350px"} >
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Name</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Mobile</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Email</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Message</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Registered Date</Typography>
-                        </Stack>
-                        <Stack flexDirection={"column"} width={"100%"}>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.name ? selectedUser?.name : "---"}</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.mobile ? selectedUser?.mobile : '---'}</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.email ? selectedUser?.email : '---'}</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.message ? selectedUser?.message : '---'}</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.registered_date ? dayjs(selectedUser?.registered_date).format("YYYY-MMMM-DD") : '---'}</Typography>
-                        </Stack>
-                    </Stack>
-                </Stack>
+               <VlsAcademyView selectedUser={selectedUser} />
             </Popup>
         </>
     );
--- src/components/sections/vls/vls-law-practice/index.tsx
+++ src/components/sections/vls/vls-law-practice/index.tsx
@@ -9,9 +9,7 @@ import VlsLawPracticeTable from './vlsTable';
 import { handleCSVDownloadData, handleXlsxDownloadData } from 'components/hooks/useExportDataToExcel';
 import { Popup } from 'components/common/Popup';
 import ConfirmAlert from 'components/common/ConfirmAlert';
-import { Typography } from '@mui/material';
-import IconifyIcon from 'components/base/IconifyIcon';
-import dayjs from 'dayjs';
+import VlsView from './vlsView';
 
 const VlsLawPracticeSection = () => {
     const { data: usersData, isLoading } = useVlsLawpracticeQuery();
@@ -21,7 +19,6 @@ const VlsLawPracticeSection = () => {
     const [selectedUserId, setSelectedUserId] = useState<number>(0);
     const [viewModal, setviewModal] = useState(false);
     const [selectedUser, setSelectedUser] = useState<any>(null);
-    const [copied, setcopied] = useState(false);
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearchText(e.target.value);
@@ -53,26 +50,6 @@ const VlsLawPracticeSection = () => {
         setviewModal(false);
         setSelectedUser(null);
     }
-    const shareurl: any = [
-        `Name : ${selectedUser?.name ?? '---'} `,
-        `Email : ${selectedUser?.email ?? '---'} `,
-        `Mobile : ${selectedUser?.mobile ?? '---'} `,
-        `Amount : ₹${selectedUser?.amount ?? '---'}`,
-        `Programm Date : ${dayjs(selectedUser?.programm_date).format("YYYY-MMMM-DD") ?? '---'}`,
-        `Registered Date : ${dayjs(selectedUser?.registered_date).format("YYYY-MMMM-DD") ?? '---'}`,
-    ]
-
-    const handleurl = async () => {
-        try {
-            await navigator.clipboard.writeText(shareurl);
-            setcopied(true);
-            setTimeout(() => {
-                setcopied(false);
-            }, 2000);
-        } catch (err) {
-            console.log(err);
-        }
-    };
 
 
 
@@ -115,57 +92,7 @@ const VlsLawPracticeSection = () => {
             </Popup>
 
             <Popup open={viewModal} onClose={handleCloseViewModal}>
-                <Stack
-                    flexDirection={'column'}
-                    justifyContent={'center'}
-                    alignItems="center"
-                    spacing={2}
-                    width={"600px"}
-                    position={"relative"}
-                    sx={{ padding: 4 }}
-                >
-                    <Typography textAlign={'center'} variant="h4" sx={{ width: '100%' }}>
-                        Vls Law Practice User
-                    </Typography>
-
-                    <Stack flexDirection={"column"} position={"absolute"} bottom={"15px"} right={"10px"} >
-                        <IconifyIcon
-                            onClick={handleurl}
-                            icon="hugeicons:copy-01"
-                            sx={{
-                                cursor: 'pointer',
-                                fontSize: "35px",
-                                backgroundColor: '#47b4ec',
-                                color: '#fff',
-                                borderRadius: '10px',
-                                padding: 0.8,
-                                '&:hover': {
-                                    backgroundColor: '#000000ff',
-                                    color: '#fff',
-                                },
-                            }}
-                        />
-                        <Typography sx={{ fontSize: "10px", fontWeight: "600", textAlign: "center" }}>{copied ? 'Copied' : ''}</Typography>
-                    </Stack>
-                    <Stack width={"100%"} display={"flex"} gap={"5px"} marginTop={"20px"}>
-                        <Stack flexDirection={"column"} width={"350px"} >
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Name</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Mobile</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Email</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Amount</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Programm Date</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>Registered Date</Typography>
-                        </Stack>
-                        <Stack flexDirection={"column"} width={"100%"}>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.name ? selectedUser?.name : "---"}</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.mobile ? selectedUser?.mobile : '---'}</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.email ? selectedUser?.email : '---'}</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.amount ? selectedUser?.amount : '---'}</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.programm_date ? dayjs(selectedUser?.programm_date).format("YYYY-MMMM-DD") : '---'}</Typography>
-                            <Typography sx={{ fontWeight: "600", marginBottom: "10px", fontSize: "15px", height: "35px" }}>: {selectedUser?.registered_date ? dayjs(selectedUser?.registered_date).format("YYYY-MMMM-DD") : '---'}</Typography>
-                        </Stack>
-                    </Stack>
-                </Stack>
+                <VlsView selectedUser={selectedUser} />
             </Popup>
         </>
     );
